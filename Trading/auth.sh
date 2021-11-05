55157bb22d06fe344afcc634d12d0bc7d3345479
Jarod <71286864+jwUTC@users.noreply.github.com>
2021-11-05 11:52:23 -0400

    Add files via upload

---
+++ Trading/auth.sh
@@ -0,0 +1,4 @@
+curl -X GET \
+    -H "APCA-API-KEY-ID: PKPEW4VT5LLM574Y2Q45" \
+    -H "APCA-API-SECRET-KEY: w5pQlSEbvveFBJI3MWDgGrNMDzYvCujolWm8LPO2"\
+    https://paper-api.alpaca.markets/v2/account
